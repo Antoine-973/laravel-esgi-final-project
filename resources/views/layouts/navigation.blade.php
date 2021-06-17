3d31d67cedde88fe8708e1ef902e5bcf87d16a2f
calintha <cal.intha77@gmail.com>
2021-06-17 22:54:42 +0200

    Fix nav user

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,15 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('dashboard')">
+                        {{ __('Profil') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')">
+                        {{ __('Vendre') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')">
+                        {{ __('Mes ventes') }}
+                    </x-nav-link>
                 </div>
             </div>
 
