14df0f4007fdcaf01a8a5308fe1bb467b8ad4d06
calintha <cal.intha77@gmail.com>
2021-07-03 17:08:27 +0200

    Fix seed type pokemon

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -47,7 +47,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Peut vous raffraichir avec ses feuilles";
         $product->price = 29.00;
-        $product->category_id = 8;
+        $product->category_id = 3;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -58,7 +58,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Déjà utilisé 5 fois pour des barbuc";
         $product->price = 29.00;
-        $product->category_id = 8;
+        $product->category_id = 1;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -69,7 +69,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Permet d'éviter les problemes de RER B";
         $product->price = 100.00;
-        $product->category_id = 8;
+        $product->category_id = 1;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -80,7 +80,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Bonne douche";
         $product->price = 15.00;
-        $product->category_id = 8;
+        $product->category_id = 2;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -91,7 +91,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "A ne pas confondre avec autre chose";
         $product->price = 1.00;
-        $product->category_id = 8;
+        $product->category_id = 3;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -102,7 +102,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "On dirait un pigeon";
         $product->price = 5.00;
-        $product->category_id = 8;
+        $product->category_id = 9;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -113,7 +113,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Capturé à Chatelet";
         $product->price = 5.00;
-        $product->category_id = 8;
+        $product->category_id = 11;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -124,7 +124,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Il sert à rien";
         $product->price = 5.00;
-        $product->category_id = 8;
+        $product->category_id = 14;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -135,7 +135,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "A déjà gagné The Voice mais chante sans arret";
         $product->price = 500.00;
-        $product->category_id = 8;
+        $product->category_id = 18;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -146,7 +146,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Peut détruire le jardin du voison que vous n'aimez pas";
         $product->price = 45.00;
-        $product->category_id = 8;
+        $product->category_id = 14;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -157,7 +157,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Il peut vous gratter le dos";
         $product->price = 45.00;
-        $product->category_id = 8;
+        $product->category_id = 10;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -168,7 +168,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Très musclé et impressionnant, il a volé ma copine";
         $product->price = 4.00;
-        $product->category_id = 8;
+        $product->category_id = 10;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -179,7 +179,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Belle décoration pour le jardin";
         $product->price = 4.00;
-        $product->category_id = 8;
+        $product->category_id = 13;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -190,7 +190,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Il est flemmard";
         $product->price = 4.00;
-        $product->category_id = 8;
+        $product->category_id = 2;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -201,7 +201,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Il pu trop";
         $product->price = 70.00;
-        $product->category_id = 8;
+        $product->category_id = 15;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -212,7 +212,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Dort et mange c'est tout";
         $product->price = 40.00;
-        $product->category_id = 8;
+        $product->category_id = 18;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
@@ -223,7 +223,7 @@ class ProductsSeeder extends Seeder
         $product->slug = $slugify->slugify($product->title);
         $product->description = "Sulfura/Electhor/Artikodin peuvent chanter les musiques de Migos";
         $product->price = 40.00;
-        $product->category_id = 8;
+        $product->category_id = 18;
         $product->user_id = User::all()->random(1)->first()->id;
         $product->image = "pikatchoum.jpg";
         $product->save();
